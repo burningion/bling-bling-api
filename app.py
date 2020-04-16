21b7dd71ff3016f45063cee9e13ad776eb46fa1b
KP Kaiser <kirk@zothcorp.com>
2020-04-16 17:02:34 -0400

    Add new requirements

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from PIL import Image
+
+import uuid
 
 app = Flask(__name__)
+
+@app.route('/place-bling')
+def place_bling():
+    return "Image will be here!"
 
 @app.route('/')
 def hello_world():
