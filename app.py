2b6e7d6a0714b9b87015287982511267d84884c5
KP Kaiser <kirk@zothcorp.com>
2020-04-20 14:56:07 -0400

    Let's see if base64 images are embeddable in Github

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from PIL import Image
 
 import uuid
 
 app = Flask(__name__)
 
-@app.route('/place-bling')
+@app.route('/place-bling', methods=['GET', 'POST'])
 def place_bling():
-    return "Image will be here!"
+    if request.method == 'GET':
+        return "You must POST a json Object in the following form: {\"image\": \"base64encodedimage\"}"
+    else:
+        return "Image will be here!"
 
 @app.route('/')
 def hello_world():
