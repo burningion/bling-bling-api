0857d6b5d6e82c3b300388159f40b1166c154e0e
KP Kaiser <kirk@zothcorp.com>
2020-04-28 12:43:40 -0400

    Throwing everything into a try except

--- app.py
+++ app.py
@@ -15,26 +15,29 @@ def place_bling():
     if request.method == 'GET':
         return "You must POST a json Object in the following form: {\"image\": \"base64encodedimage\"}"
     else:
-        image = request.json['image']
-        spot_count = request.json['spots']
+        try:
+            image = request.json['image']
+            spot_count = request.json['spots']
 
-        image_data = re.sub('^data:image/.+;base64', '', image)
-        im = Image.open(BytesIO(base64.b64decode(image_data)))
-        paster = Image.open('images/bling.gif')
-        im.thumbnail((500, 500))
-        spots = []
-        for i in range(spot_count):
-            spots.append((random.randint(0,500), random.randint(0,500)))
-        frames = []
-        for frame in ImageSequence.Iterator(paster):
-            frame = frame.convert('RGBA')
-            frame.thumbnail((40, 40))
-            curr = im.copy().convert('RGBA')
-            for spot in spots:
-                curr.paste(frame, spot, mask=frame)
-            frames.append(curr)
-        frames[0].save('images/test.gif', save_all=True, append_images=frames[1:], loop=0) # save_all necessary for animation
-        return "Image created!"
+            image_data = re.sub('^data:image/.+;base64', '', image)
+            im = Image.open(BytesIO(base64.b64decode(image_data)))
+            paster = Image.open('images/bling.gif')
+            im.thumbnail((500, 500))
+            spots = []
+            for i in range(spot_count):
+                spots.append((random.randint(0,500), random.randint(0,500)))
+                frames = []
+            for frame in ImageSequence.Iterator(paster):
+                frame = frame.convert('RGBA')
+                frame.thumbnail((40, 40))
+                curr = im.copy().convert('RGBA')
+                for spot in spots:
+                    curr.paste(frame, spot, mask=frame)
+                frames.append(curr)
+            frames[0].save('images/test.gif', save_all=True, append_images=frames[1:], loop=0) # save_all necessary for animation
+            return "Image created!"
+        except:
+            return "Malformed JSON request!"
 
 @app.route('/')
 def hello_world():
