61515a9b51c777b2035bdb477a727430f4fdea20
Ursula Huang <ursula1207@hotmail.com>
2020-08-21 05:15:50 -0400

    Return a base64encoded image instead of feedback string

--- app.py
+++ app.py
@@ -13,31 +13,40 @@ app = Flask(__name__)
 @app.route('/place-bling', methods=['GET', 'POST'])
 def place_bling():
     if request.method == 'GET':
-        return "You must POST a json Object in the following form: {\"image\": \"base64encodedimage\"}"
+        return "You must POST a json Object in the following form: {\"spots\": number, \"image\": \"base64encodedimage\"}"
     else:
         try:
-            image = request.json['image']
             spot_count = request.json['spots']
-
+            image = request.json['image']
             image_data = re.sub('^data:image/.+;base64', '', image)
-            im = Image.open(BytesIO(base64.b64decode(image_data)))
-            paster = Image.open('images/bling.gif')
-            im.thumbnail((500, 500))
-            spots = []
-            for i in range(spot_count):
-                spots.append((random.randint(0,500), random.randint(0,500)))
-                frames = []
-            for frame in ImageSequence.Iterator(paster):
-                frame = frame.convert('RGBA')
-                frame.thumbnail((40, 40))
-                curr = im.copy().convert('RGBA')
-                for spot in spots:
-                    curr.paste(frame, spot, mask=frame)
-                frames.append(curr)
-            frames[0].save('images/test.gif', save_all=True, append_images=frames[1:], loop=0) # save_all necessary for animation
-            return "Image created!"
         except:
-            return "Malformed JSON request!"
+            return "Malformed JSON request! Given: " + str(request.json)
+
+        im = Image.open(BytesIO(base64.b64decode(image_data)))
+        paster = Image.open('images/bling.gif')
+        im.thumbnail((500, 500))
+        width, height = im.size
+
+        bling_width, bling_height = 40, 40
+
+        spots = []
+        for i in range(spot_count):
+            spots.append((random.randint(0,width - bling_width), random.randint(0,height - bling_height)))
+        frames = []
+        for frame in ImageSequence.Iterator(paster):
+            frame = frame.convert('RGBA')
+            frame.thumbnail((bling_width, bling_height))
+            curr = im.copy().convert('RGBA')
+            for spot in spots:
+                curr.paste(frame, spot, mask=frame)
+            frames.append(curr)
+
+        buffered = BytesIO()
+        frames[0].save(buffered, format="GIF", save_all=True, append_images=frames[1:], loop=0) # save_all necessary for animation
+        img_str = base64.b64encode(buffered.getvalue())
+
+        print("Image created!")
+        return img_str
 
 @app.route('/')
 def hello_world():
